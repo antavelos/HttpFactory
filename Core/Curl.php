f301170fd14a35a46610edebff8964d2066dbf57
Alexandros Ntavelos <a.ntavelos@gmail.com>
2013-09-20 13:09:09 +0200

    More modifications

---
+++ Core/Curl.php
@@ -0,0 +1,85 @@
+<?php
+// HttpFactory/Core/Curl.php
+
+namespace HttpFactory\Core;
+
+/**
+ * Wraps up the basic curl functions
+ * 
+ * @author Alexandros Ntavelos
+ * @package HttpFactory.Core
+ */
+class Curl
+{
+    /**
+     * The cURL handler
+     * @var cURL handle $curlHandler
+     * 
+     */
+    private $handler;
+
+    /**
+     * Initializes the handler
+     *
+     * @throws Exception If cURL handler initialization fails
+     */
+    public function __construct()
+    {
+        $this->handler = curl_init();
+        if ($this->handler === false) {
+            throw new \Exception("Error on cURL handler initialization:" . 
+                curl_error($this->handler));
+        }
+    }
+
+    /**
+     * Closes the cURL session
+     */
+    public function __destruct()
+    {
+        curl_close($this->handler);
+    }
+
+    /**
+     * Performs a cURL session
+     * @throws Exception If cURL session execution fails
+     */
+    protected function exec()
+    {
+        $result = curl_exec($this->handler);
+        if ($result === false) {
+            throw new \Exception("Error on cURL session execution:" . 
+                curl_error($this->handler));
+        }
+    }
+
+    /**
+     * Returns request info
+     * @throws Exception If cURL info retrieval fails
+     */
+    protected function info($infoCode)
+    {
+        $result = curl_getinfo($this->handler, $infoCode);
+        if ($result === false) {
+            throw new \Exception("Error on cURL info retrieval:" . 
+                curl_error($this->handler));
+        }
+    }
+
+    /**
+     * Sets an option on the given cURL session handle.
+     * 
+     * @param string $option
+     * @param mixed $value
+     *
+     * @return  boolean true or false depending on the outcome of the function
+     */
+    protected function addOption($option, $value)
+    {
+        if(curl_setopt($this->handler, $option, $value) === false) {
+            throw new \Exception("Error on setting option " . $option . 
+                " with value " . $value . "for cURL transfer:" . 
+                curl_error($this->handler));
+        } 
+    }
+}
---
+++ Http/Request.php
@@ -0,0 +1,199 @@
+<?php
+// HttpFactory/Http/Request.php
+
+namespace HttpFactory\Core;
+
+use HttpFactory\Core\Curl;
+
+/**
+ * Implements the HTTP request 
+ * 
+ * @author Alexandros Ntavelos
+ * @package HttpFactory.Http
+ */
+class Request extends Curl
+{
+    /**
+     * The HTTP request method
+     * @var string $method
+     */
+    private $method;
+
+    /**
+     * The request URL
+     * @var string $url
+     */
+    private $url;
+
+    /**
+     * The HTTP request headers
+     * @var array $headers
+     */
+    private $headers;
+
+    /**
+     * The HTTP request body
+    * @var string $body
+    */
+    private $body;
+
+    /**
+     * The constructor of the Request
+     *
+     * @param string $method The HTTP request method
+     */
+    public function __construct($method)
+    {        
+        parent::__construct();
+        $this->optionsPerMethod($method);        
+        $this->method = $method;
+        $this->headers = array();
+        $this->addOption(CURLOPT_RETURNTRANSFER, true); // returns the result on
+                                                        // success
+    }
+
+    /**
+     * Implements the HTTP request 
+     *
+     * @return array The response body and code
+     */
+    public function send()
+    {
+        $this->checkVariables();
+        $result = $this->exec();
+        $code = $this->info(CURLINFO_HTTP_CODE);
+
+        return array(
+            "body" => $result,
+            "code" => $code,
+        );
+    }
+
+    /**
+     * Gets the HTTP request method.
+     *
+     * @return string $method
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * Sets the request URL and adds a CURLOPT_URL option.
+     *
+     * @param string $url The HTTP request URL
+     *
+     * @return self
+     */
+    public function setUrl($url)
+    {
+        $this->addOption(CURLOPT_URL, $url);
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Gets the request URL.
+     *
+     * @return string $url
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Sets the HTTP request body and adds a CURLOPT_POSTFIELDS option for
+     * POST or PUT methods 
+     * 
+     * @param string $body $body the body
+     *
+     * @return self
+     */
+    public function setBody($body)
+    {
+        if ($this->method == "POST" || $this->method == "PUT") {
+            $this->addOption(CURLOPT_POSTFIELDS, $body);
+        }
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Gets the HTTP request body.
+     *
+     * @return string $body
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Sets an HTTP request header.
+     *
+     * @param array $header The header to be added
+     *
+     * @return self
+     */
+    public function setHeaders($header)
+    {
+        $this->addOption(CURLOPT_HTTPHEADER, $header);
+        $this->headers[] = $header;
+
+        return $this;
+    }
+
+    /**
+     * Gets the The HTTP request headers.
+     *
+     * @return array $headers
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * Assigns the appropriate header according to the HTTP request method
+     *
+     * @param  string $method The HTTP request method
+     */
+    private function optionsPerMethod($method)
+    {
+        switch ($method) {
+            case "GET":
+                break;
+            case "POST":
+                $this->addOption(CURLOPT_POST, true);
+                break;
+            case "PUT":
+                $this->addOption(CURLOPT_CUSTOMREQUEST, "PUT");
+                break;
+            case "DELETE":
+                $this->addOption(CURLOPT_CUSTOMREQUEST, "DELETE");
+                break;
+            default:
+                throw new \Exception("Invalid request method");
+        }
+    }
+
+    /**
+     * Checks whether body and url are set
+     */
+    private function checkVariables()
+    {
+        if (!isset($this->url)) {
+            throw new \Exception("No request URL is specified");
+        }
+
+        if ($this->method == "POST" || $this->method == "PUT") {
+            if (!isset($this->body)) {
+                throw new \Exception("No request body is specified");
+            }
+        }
+    }
+}
